af2a09766a1b8645ec3bc4a3f597041062d17278
aman0sriv <21bec014@iiitdmj.ac.in>
2024-08-28 03:10:47 +0530

    Revert "Snake and Ladder"
    This reverts commit 9863f7250ca45f1f17ff9da4cf54ee22caadd0fb.

--- snake_ladder/Ladders.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Jumps.hpp"
-#include <unordered_map>
-using namespace std;
-class Ladders : public Jumps {
-  public:
-    Ladders(const std::unordered_map<int,int> & ladders) : Jumps(ladders) {}
-    Ladders(){}
-};
--- snake_ladder/Snakes.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Jumps.hpp"
-#include <unordered_map>
-using namespace std;
-class Snakes : public Jumps {
-  public:
-    Snakes(const std::unordered_map<int,int> & snakes) : Jumps(snakes) {}
-    Snakes(){}
-};
